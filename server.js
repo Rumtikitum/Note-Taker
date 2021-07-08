ddc33bbe2f73d685994c5e29f69c0f02a2d52c68
Richie Tauch <rtauc001@gmail.com>
2021-07-07 22:52:08 -0700

    establishing routes to local html files to create communication

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//est some constants
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -8,3 +9,22 @@ const app = express();
 app.use(express.static("./Develop/public"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
+
+
+
+//set up routes...connecting to html files
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname,"./Develop/public/notes.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname,"./Develop/public/index.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname,"./Develop/public/index.html"));
+});
