6c2cee66f02ca51a460b2a5e45aa78ac552c4810
Richie Tauch <rtauc001@gmail.com>
2021-07-08 19:56:53 -0700

    functioning code. Will try to clean up

--- server.js
+++ server.js
@@ -1,34 +1,74 @@
-//est some constants
-const fs = require('fs');
-const path = require('path');
+//creating a server
 const express = require('express');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
-
-app.use(express.static("./Develop/public"))
+const PORT = 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
 
-//Read and Write
-router.get("/notes", function(req, res){
-    notes.getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
-})
-router.post("/notes", function(req, res){
-    notes.addNotes(req.body)
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
-})
-router.delete("/notes/:id", function(req, res){
-    notes.removeNote(req.params.id)
-    .then(() => res.json({ok: true}))
-    .catch(err => res.status(500).json(err));
-})
+const path = require('path');
+
+//required to write and create promise 
+const fs = require('fs');
+const promise = require('util').promisify;
+const read = promise(fs.readFile);
+const write = promise(fs.writeFile);
+
+//--------------------------------------------------------------------------
+//Get!!
+app.get("/api/notes", function(req, res) {
+    read("./develop/db/db.json", "utf8")
+    .then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+      })
+  }); 
+  
+  
+//Post!!!
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    read("./develop/db/db.json", "utf8")
+    .then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        write("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
+  });
 
 
-//set up routes...connecting to html files
+//Delete...sorta...
+app.delete("/api/notes/:id", function(req, res) {
+    const deleteId = parseInt(req.params.id);
+    read("./develop/db/db.json", "utf8")
+    .then(function(data) {
+      const notes = [].concat(JSON.parse(data));
+      const replacement = []
+      for (let i = 0; i<notes.length; i++) {
+        if(deleteId !== notes[i].id) {
+          replacement.push(notes[i])
+        }
+      }
+      return replacement
+    }).then(function(notes) {
+      write("./develop/db/db.json", JSON.stringify(notes))
+      res.send('rewrite');
+    })
+  })
+//-----------------------------------------------------------------------------------
+
+  
+//Server showing public folder files...front end
+app.use(express.static("./Develop/public"))
+
+
+//set up routes...loops user when they attempt to go anywhere else on the 5000 aside from notes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname,"./Develop/public/notes.html"));
 });
