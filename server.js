45fd6c8b2afcb85a599b6f1a9cf386fc3bbfbfea
Richie Tauch <rtauc001@gmail.com>
2021-07-09 22:15:12 -0700

    fixing for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
+    console.log(`App listening on PORT ${PORT}` || 5000);
   });
 
 const path = require('path');
